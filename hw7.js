7cfcd4f3d99b4733eec511a1dd935e89205ab1b4
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-12-17 21:18:01 -0500

    Homework 7 and 8 changes

--- hw7.js
+++ hw7.js
@@ -1,133 +1,49 @@
-function getWeather(coords, callback) {
-	  var url = 'http://api.openweathermap.org/data/2.5/weather?q=Laurel,MD';
-	  $.ajax({
-	    dataType: "jsonp",
-	    url: url,
-	    jsonCallback: 'jsonp',
-	    data: { lat: coords[0], lon: coords[1] },
-	    cache: false,
-	    success: function (data) {
-	    	callback(data);
-	    }
-	  });
+$(document).ready(function() {
+	var getWeather = function(coords, callback) {
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=Laurel,MD';
+		$.ajax({
+			dataType: "jsonp",
+			url: url,
+			jsonCallback: 'jsonp',
+			data: {
+				lat: coords[0],
+				lon: coords[1]
+			},
+			cache: false,
+			success: function(data) {
+				callback(data);
+			}
+		});
 	}
 
-	$(document).ready(function () {
+	var teams = [{
+			Teams: "Laurel, Maryland",
+			Long: -0.800299,
+			Lat: 51.6306,
+			Weather: "http://api.openweathermap.org/data/2.5/weather?q=Laurel,MD"
+		}
 
-			var teams = [
-				{
-				  Teams: "Meryland",
-				  Long: -0.800299,
-				  Lat: 51.6306,
-				  Weather: "http://api.openweathermap.org/data/2.5/weather?q=Laurel,MD"
-				}
-				
-			];
+	];
 
-	    $("#btn382").click(function () {
+	$("#btn382").click(function() {
 
-	    	for (var team in teams) {
-	    		var obj = teams[team];
-	    		(function (team) {
-		    		coords = [team.Lat, team.Long]
-			      getWeather(coords, function(data) {
-							var html = [];
-							html.push('<div>')
-			        html.push('City: ', team.Teams, ', ');
-			        html.push('Lat: ', data.coord.lat, ', ');
-			        html.push('Lon: ', data.coord.lon, ', ');
-			        html.push('Weather: ', data.weather[0].description);
-							html.push('</div>')
-			        $("#div381").append(html.join('')).css("background-color", "orange");
-			      });
-	    		}(obj));
-	    	}
-
-	    });
+		for (var team in teams) {
+			var obj = teams[team];
+			(function(team) {
+				coords = [team.Lat, team.Long]
+				getWeather(coords, function(data) {
+					var html = [];
+					html.push('<div>')
+					html.push('City: ', team.Teams, ', ');
+					html.push('Lat: ', data.coord.lat, ', ');
+					html.push('Lon: ', data.coord.lon, ', ');
+					html.push('Weather: ', data.weather[0].description);
+					html.push('</div>')
+					$("#div381").append(html.join('')).css("background-color", "orange");
+				});
+			}(obj));
+		}
 
 	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
